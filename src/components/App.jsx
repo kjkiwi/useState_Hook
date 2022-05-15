cbd1c37445ad801b19919e1eff23495445741430
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-16 11:38:58 +1200

    Separate index.js into components Need to do this so can use a react hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function App() {
-  return <div />;
+  var count = 0;
+
+  function increase() {
+    count++;
+    ReactDOM.render(
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick={increase}>+</button>
+      </div>,
+      document.getElementById("root")
+    );
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
